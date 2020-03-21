14de62276458d9edb99342354de90712266ba922
Mohamed <m.khobba@picaxel.com>
2020-03-21 02:57:59 +0100

    - Using Apollo with React.

---
+++ frontend/components/items.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class items extends Component {
+    render() {
+        return (
+            <div>
+                Thes are items  !!!
+            </div>
+        )
+    }
+}
+
+export default items
